009e542bee61221a0c223cd7f5301f5c767a6ce3
iamtash <natashachernis@gmail.com>
2019-09-26 19:07:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Define 4 const called: append, prepend, removeLast and removeFirst, and set all to an initial value of ["Milo", "Otis", "Garfield"]
 
 const append = ["Milo", "Otis", "Garfield"];
-const prepend = ["Milo", "Otis", "Garfield"];
-const removeLast = ["Milo", "Otis", "Garfield"];
-const removeFirst = ["Milo", "Otis", "Garfield"];
+const prepend = append.slice();
+const removeLast = append.slice();
+const removeFirst = append.slice();
 
 append.push('Odie');
 removeLast.pop();
